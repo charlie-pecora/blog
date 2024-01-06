9c87a37037c42d28193a17fc0cd18772680a9528
charlie-pecora <charlie.pecora@gmail.com>
2024-01-06 08:09:06 -0800

    polls tutorial WIP

---
+++ src/blog/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present charlie-pecora <charlie.pecora@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present charlie-pecora <charlie.pecora@gmail.com>
+#
+# SPDX-License-Identifier: MIT
